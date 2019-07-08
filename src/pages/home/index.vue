2c14f277b958b8195f18db2c786e8d3e9df3ace0
lml <soalin228@163.com>
2019-07-08 23:21:12 +0800

    header 开发完成

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,19 +1,35 @@
 <template>
-  <div>
-    home
-    <router-view>123</router-view>
+  <div class="home">
+    <header class="g-header-container">
+      <home-header></home-header>
+    </header>
+    <div></div>
+    <div class="g-backtop-container"></div>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
+import HomeHeader from './header'
+
 export default {
   name: 'Home',
+  components: {
+    HomeHeader
+  },
   data () {
     return {}
   }
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+  @import "~assets/scss/mixins";
 
+  .home {
+    overflow: hidden;
+    width: 100%;
+    height: 100%;
+    background-color: $bgc-theme;
+  }
 </style>
