18740deffe6bf2dbc075ec9b9d222cc3a88bac4b
lml <soalin228@163.com>
2019-07-22 23:34:03 +0800

    category content api取消前一次请求

--- src/api/category.js
+++ src/api/category.js
@@ -1,10 +1,17 @@
 import axios from 'axios'
 import {SUCC_CODE, TIMEOUT} from './config'
 
+const CancelToken = axios.CancelToken
+let cancel
 // 获取内容数据--ajax
 export const getCategoryContent = (id) => {
+  cancel && cancel('取消了前一次的请求！')
+  cancel = null
   return axios.get(`http://www.imooc.com/api/category/content/${id}`, {
-    timeout: TIMEOUT
+    timeout: TIMEOUT,
+    cancelToken: new CancelToken((c) => { // 取消前一次的请求
+      cancel = c
+    })
   }).then(res => {
     if (res.data.code === SUCC_CODE) {
       return res.data.content
@@ -12,7 +19,9 @@ export const getCategoryContent = (id) => {
 
     throw new Error('没有成功获取到数据！')
   }).catch(err => {
-    if (err) { // 取消前一次的请求
+    if (axios.isCancel(err)) { // 捕获取消前一次的请求的
+      console.log(err)
+    } else {
       console.log(err)
     }
   })
